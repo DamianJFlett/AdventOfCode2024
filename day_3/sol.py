f1a0d77cf8e3fb20e899f9ff0c7a808980e11b16
Damian Flett <damian.flett@outlook.com>
2024-12-03 20:15:49 +1000

    part 2 day 3

--- day_3/sol.py
+++ day_3/sol.py
@@ -5,9 +5,17 @@ for l in f:
     text += l
 i = 0
 total = 0 
-muls = re.findall(r'mul\(\d+,\d+\)', text)
+muls = re.findall(r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)', text)
+enabled = True
 for mul in muls:
-    t1, t2 = mul.split(",")
-    x, y = int(t1.split("(")[1]), int(t2.split(")")[0])
-    total += x*y
+    if mul == "do()":
+        enabled = True
+        continue
+    if mul == "don't()":
+        enabled = False
+        continue
+    if enabled: # commend this and unindent for part 1
+        t1, t2 = mul.split(",")
+        x, y = int(t1.split("(")[1]), int(t2.split(")")[0])
+        total += x*y
 print(total)
