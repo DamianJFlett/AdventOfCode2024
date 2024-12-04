e59a0f9f343358c5891282ac8db4d971dcf06dd7
Damian Flett <damian.flett@outlook.com>
2024-12-04 17:21:35 +1000

    day 4 part 1

---
+++ day_4/sol.py
@@ -0,0 +1,49 @@
+import re
+f = open("input.txt", "r")
+# method - create matrices to search through with a regex, these should be the matrix originally
+# rotated 45 degrees (so that something like [[1,2], [3,4]] would be [[1]. [2,3], [4]]) and the transpose
+# to search every direction. Storage inefficient but the search is sufficiently small. 
+ws = []
+for l in f:
+    ws.append(l[:-1])
+
+ws_transpose = [""] * len(ws[0])
+for (lindex, l) in enumerate(ws):
+    for (cindex, c) in enumerate(l):
+        ws_transpose[cindex] = ws_transpose[cindex] + c
+
+
+# to construct these, lets notice something about this diagonal matrix in n dimensions - 
+# nth row has all the a_ij s.t. i+j=n+1. Easy here since matrix is square. To see this, notice that moving along a diagonal 
+# subtracts 1 from the row and adds 1 to the column. 
+
+#note - I'm sure there's a way to do this that directly accessess the necessary matrix elements, maybe come back and do it!
+ws_diagonal = [""] * (2*len(ws)-1)
+for n in range(2 * len(ws) - 1):  #rows of diagonal
+    for i in range(len(ws)):  # rows of original
+        for j in range(len(ws[0])):  # columns of original
+            if i + j == n: 
+                ws_diagonal[n] += str(ws[i][j]) 
+
+
+ws_diagonal2 = [""] * (2 * len(ws) - 1)
+for n in range(2 * len(ws) - 1):  # rows of diagonal
+    for i in range(len(ws)):  # rows of original
+        for j in range(len(ws[0])):  # columns of original
+            if j - i == n - (len(ws) - 1):  #this check is different for the other diagonal
+                ws_diagonal2[n] += str(ws[i][j])
+
+total_xmas = 0
+for row in ws:
+    total_xmas += len(re.findall(r'XMAS', row))
+    total_xmas += len(re.findall(r'SAMX', row))
+for row in ws_transpose:
+    total_xmas += len(re.findall(r'XMAS', row))
+    total_xmas += len(re.findall(r'SAMX', row))
+for row in ws_diagonal:
+    total_xmas += len(re.findall(r'XMAS', row))
+    total_xmas += len(re.findall(r'SAMX', row))
+for row in ws_diagonal2:
+    total_xmas += len(re.findall(r'XMAS', row))
+    total_xmas += len(re.findall(r'SAMX', row))
+print(total_xmas)
