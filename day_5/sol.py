90d6f879750c128c42b962e74abace56d378111e
Damian Flett <damian.flett@outlook.com>
2024-12-05 18:13:26 +1000

    part 1 day 5 complete

---
+++ day_5/sol.py
@@ -0,0 +1,42 @@
+f = open("input.txt", "r")
+
+
+line_num = 0
+raw_relations = []
+raw_orders = []
+relations = {}
+orders = []
+total = 0
+
+for l in f:
+    line_num += 1
+    if line_num <= 1176:
+        raw_relations.append(l[:-1])
+    elif line_num == 1177:
+        continue
+    else:
+        raw_orders.append(l[:-1])
+
+
+for rel in raw_relations:
+    relations[int(rel[0:2])] = []
+    relations[int(rel[3:5])] = []
+for rel in raw_relations:
+    relations[int(rel[0:2])].append(int(rel[3:5]))
+
+#list comprehension is gods gift to man
+orders = [[int(num) for num in order.split(",")] for order in raw_orders]
+
+
+def get_middle_element(order):
+    return order[len(order) // 2]
+
+for order in orders:
+    valid = True
+    for (index, num) in enumerate(order):
+        for i in range(index):
+            if order[i] in relations[num]:
+                valid = False
+    if valid: 
+        total += get_middle_element(order)
+print(total)
