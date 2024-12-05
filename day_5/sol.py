ae825065b08a4ebe290d1d52178220e0d6fc7afc
Damian Flett <damian.flett@outlook.com>
2024-12-05 20:11:55 +1000

    day 5 part 2 complete

--- day_5/sol.py
+++ day_5/sol.py
@@ -31,6 +31,47 @@ orders = [[int(num) for num in order.split(",")] for order in raw_orders]
 def get_middle_element(order):
     return order[len(order) // 2]
 
+"""
+Given an unordered list, returns what it would be if correctly ordered
+"""
+def reorder(order: list[int], rels: dict[int: int]):
+    #algorithm - for each element, compare it to each other element. 
+    #should this be before it? if so, move it back. Modification is inplace
+    # for i in order:
+    #     for j in order:
+    #         for k in range(100):
+    #             if i in rels[j]: #if i supposed to be before j
+    #                 order.remove(i)
+    #                 order.insert(order.index(j), i)
+    # return order
+    return bubbleSort(order)
+
+"""
+The following function taken from https://www.geeksforgeeks.org/bubble-sort-algorithm/
+and modified to use relevant comparator
+"""
+# Optimized Python program for implementation of Bubble Sort
+def bubbleSort(arr):
+    n = len(arr)
+    
+    # Traverse through all array elements
+    for i in range(n):
+        swapped = False
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j+1] in relations[arr[j]]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if (swapped == False):
+            break
+    return arr
+
+
 for order in orders:
     valid = True
     for (index, num) in enumerate(order):
@@ -39,4 +80,10 @@ for order in orders:
                 valid = False
     if valid: 
         total_valid += get_middle_element(order)
+    else:
+        total_invalid += get_middle_element(reorder(order, relations))
+
+
 print("Part one solution is", total_valid)
+print("Part 2 soltution is ", total_invalid)
+
