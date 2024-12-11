fb8333a2217a278262901b06a7035113f776254c
Damian Flett <damian.flett@outlook.com>
2024-12-11 15:33:03 +1000

    day 11 complete

---
+++ day_11/sol.py
@@ -0,0 +1,45 @@
+from bit_vector import BitVector
+from collections import defaultdict
+f = open("input.txt", "r")
+nums = [41078, 18, 7, 0, 4785508, 535256, 8154, 447]
+
+def blink_line(nums):
+    l = []
+    for i in nums:
+        l+= blink_stone(i)
+    return l
+
+def blink_stone(num):
+    if num == 0:
+        return [1]
+    elif not (len(str(num)) % 2):
+        return [int(str(num)[:len(str(num))// 2 ]), int(str(num)[len(str(num)) // 2:])]
+    else:
+        return [num * 2024]
+    
+
+x = nums
+for i in range(25):
+    x = blink_line(x)
+print("part 1 solution is ", len(x))
+
+counts = defaultdict(int)
+for i in nums:
+    counts[i] += 1
+# dp kinda?
+for i in range(75):
+    updated_counts = defaultdict(int)
+    for num in counts:
+        if num == 0:
+            updated_counts[1] += counts[num]
+        elif not (len(str(num)) % 2):
+            updated_counts[int(str(num)[:len(str(num))// 2 ])] += counts[num]
+            updated_counts[int(str(num)[len(str(num)) // 2:])] += counts[num]
+        else:
+            updated_counts[num*2024] = counts[num]
+    counts = updated_counts
+total2 = 0
+for i in counts.values():
+    total2 += i
+print("part 2 soln is ", total2)
+
